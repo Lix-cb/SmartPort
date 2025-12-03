a52d1919a651e57e253a716b4c3ba3868c922c15
Jorge Cardenas Blanco <a01647246@tec.mx>
2025-12-03 17:49:01 -0600

    Change CSS import from AdminCamara.css to app.css

--- frontend/src/pages/AdminCamara.jsx
+++ frontend/src/pages/AdminCamara.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import "./AdminCamara.css";
+import "./app.css";
 
 const API_URL = import.meta.env. VITE_API_URL || "http://localhost:5000";
 
