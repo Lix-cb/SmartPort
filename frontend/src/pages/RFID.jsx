f616a11af0e4509791a796cb3195855314f14db7
Jorge Cardenas Blanco <a01647246@tec.mx>
2025-12-02 18:51:05 -0600

    Fix import path for CSS file in RFID component

--- frontend/src/pages/RFID.jsx
+++ frontend/src/pages/RFID.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../App.css";
+import "../app.css";
 
 const API_URL = "http://localhost:5000";
 
